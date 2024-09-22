a94529722297ca027bf9dda318aad5be825f2187
Kevyn-Cavalcanti <contacontaconta0002@gmail.com>
2024-09-22 15:47:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require 'src/db.php';
+require 'src/controller.php';
+
+$db = (new Database())->getConnection();
+$controller = new controller($db);
+
+// Definir a rota com base na URL e no método HTTP
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Roteamento manual
+if ($path == '/produtos' && $method == 'GET') {
+    $controller->getProdutos();
+} elseif (preg_match('/\/produtos\/(\d+)/', $path, $matches) && $method == 'GET') {
+    $id = $matches[1];
+    $controller->getProduto($id);
+} elseif ($path == '/produtos' && $method == 'POST') {
+    $controller->createProduto();
+} elseif (preg_match('/\/produtos\/(\d+)/', $path, $matches) && $method == 'PUT') {
+    $id = $matches[1];
+    $controller->updateProduto($id);
+} elseif (preg_match('/\/produtos\/(\d+)/', $path, $matches) && $method == 'DELETE') {
+    $id = $matches[1];
+    $controller->deleteProduto($id);
+} elseif ($path == '/logs' && $method == 'GET') {
+    $controller->getLogs();
+} elseif ($path == '/logs' && $method == 'DELETE') {
+    $controller->clearLogs();
+} else {
+    // Resposta para rota não encontrada
+    http_response_code(404);
+    echo json_encode(["message" => "Rota não encontrada"]);
+}
